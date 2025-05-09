Quarx2k/OpenWRT-BE7000

commit a9340bcc02d7a8d5fef9c7eed050e19e4ea43ef5

kernel: modules: split package fs-fscache
The module kmod-fs-fscache was merged into kmod-fs-netfs since
6.12 LTS kernel[1]. Therefore, we have to split fs-cachefiles
into a separate package. This patch also removed some outdated
config symbols that have already been removed in 5.15 kernel:

CONFIG_FSCACHE_OBJECT_LIST [2]
CONFIG_{FSCACHE,CACHEFILES}_HISTOGRAM [3]

[1] https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=linux-6.12.y&id=915cd30cdea8811cddd8f59e57dd9dd0a814b76c
[2] https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=linux-5.15.y&id=58f386a73f16cea1f78e8466cc5c402eb7f6fcf8
[3] https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=linux-5.15.y&id=6ae9bd8bb037b7c422bafde746f2338a716f6058
Signed-off-by: Shiji Yang <yangshiji66@outlook.com>
Link: https://github.com/openwrt/openwrt/pull/18661
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/fs.mk
@@ -31,7 +31,7 @@ define KernelPackage/fs-afs
   SUBMENU:=$(FS_MENU)
   TITLE:=Andrew FileSystem client
   DEFAULT:=n
-  DEPENDS:=+kmod-rxrpc +kmod-dnsresolver +kmod-fs-fscache
+  DEPENDS:=+kmod-rxrpc +kmod-dnsresolver +LINUX_6_6:kmod-fs-fscache +!LINUX_6_6:kmod-fs-netfs
   KCONFIG:=\
 	CONFIG_AFS_FS=m \
 	CONFIG_AFS_DEBUG=n \
@@ -81,6 +81,22 @@ define KernelPackage/fs-btrfs/description
 endef
 
 $(eval $(call KernelPackage,fs-btrfs))
+
+
+define KernelPackage/fs-cachefiles
+  SUBMENU:=$(FS_MENU)
+  TITLE:=Filesystem caching on files
+  DEPENDS:=LINUX_6_6:kmod-fs-fscache !LINUX_6_6:kmod-fs-netfs
+  KCONFIG:=\
+	CONFIG_CACHEFILES \
+	CONFIG_CACHEFILES_DEBUG=n \
+	CONFIG_CACHEFILES_ERROR_INJECTION=n \
+	CONFIG_CACHEFILES_ONDEMAND=n
+  FILES:= $(LINUX_DIR)/fs/cachefiles/cachefiles.ko
+  AUTOLOAD:=$(call AutoLoad,30,cachefiles)
+endef
+
+$(eval $(call KernelPackage,fs-cachefiles))
 
 
 define KernelPackage/fs-smbfs-common
@@ -262,23 +278,13 @@ $(eval $(call KernelPackage,fs-f2fs))
 define KernelPackage/fs-fscache
   SUBMENU:=$(FS_MENU)
   TITLE:=General filesystem local cache manager
-  DEPENDS:=+kmod-fs-netfs
+  DEPENDS:=@LINUX_6_6 +kmod-fs-netfs
   KCONFIG:=\
-	CONFIG_FSCACHE@lt6.12 \
-	CONFIG_FSCACHE=y@ge6.12 \
+	CONFIG_FSCACHE \
 	CONFIG_FSCACHE_STATS=y \
-	CONFIG_FSCACHE_HISTOGRAM=n \
-	CONFIG_FSCACHE_DEBUG=n \
-	CONFIG_FSCACHE_OBJECT_LIST=n \
-	CONFIG_CACHEFILES \
-	CONFIG_CACHEFILES_DEBUG=n \
-	CONFIG_CACHEFILES_HISTOGRAM=n \
-	CONFIG_CACHEFILES_ERROR_INJECTION=n \
-	CONFIG_CACHEFILES_ONDEMAND=n
-  FILES:= \
-	$(LINUX_DIR)/fs/fscache/fscache.ko@lt6.12 \
-	$(LINUX_DIR)/fs/cachefiles/cachefiles.ko
-  AUTOLOAD:=$(call AutoLoad,29,fscache cachefiles)
+	CONFIG_FSCACHE_DEBUG=n
+  FILES:= $(LINUX_DIR)/fs/fscache/fscache.ko
+  AUTOLOAD:=$(call AutoLoad,29,fscache)
 endef
 
 $(eval $(call KernelPackage,fs-fscache))
@@ -423,7 +429,10 @@ $(eval $(call KernelPackage,fs-msdos))
 define KernelPackage/fs-netfs
   SUBMENU:=$(FS_MENU)
   TITLE:=Network Filesystems support
-  KCONFIG:= CONFIG_NETFS_SUPPORT
+  KCONFIG:=
+	CONFIG_NETFS_SUPPORT \
+	CONFIG_FSCACHE=y@ge6.12 \
+	CONFIG_FSCACHE_STATS=y@ge6.12
   FILES:=$(LINUX_DIR)/fs/netfs/netfs.ko
   AUTOLOAD:=$(call AutoLoad,28,netfs)
 endef

commit e2210b55c1c205dcec54585d6e2b550e15583041

kernel: modules: fs: add missing backslash
Commit a9340bcc02 ("kernel: modules: split package fs-fscache") modified
the KCONFIG section of KernelPackage/fs-netfs but missed to add a
backslash to escape a linebreak. Add it now.

Fixes: a9340bcc02 ("kernel: modules: split package fs-fscache")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/kernel/linux/modules/fs.mk
@@ -429,7 +429,7 @@ $(eval $(call KernelPackage,fs-msdos))
 define KernelPackage/fs-netfs
   SUBMENU:=$(FS_MENU)
   TITLE:=Network Filesystems support
-  KCONFIG:=
+  KCONFIG:= \
 	CONFIG_NETFS_SUPPORT \
 	CONFIG_FSCACHE=y@ge6.12 \
 	CONFIG_FSCACHE_STATS=y@ge6.12