Quarx2k/OpenWRT-BE7000

commit b529d46405e2b8381b01d66e989ae9014b5a1be4

x86: modules: add packages for Intel LPSS drivers
Add driver packages for Intel Low-Power Subsystem devices which are part
of some Intel chipsets. They are mainly needed to have access to the I2C
bus used for HID devices.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/x86/modules.mk
@@ -99,6 +99,62 @@ endef
 
 $(eval $(call KernelPackage,ib700-wdt))
 
+
+define KernelPackage/intel-lpss
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=Intel LPSS common
+  DEPENDS:=@TARGET_x86
+  KCONFIG:=CONFIG_INTEL_LPSS
+  FILES:=$(LINUX_DIR)/drivers/mfd/intel-lpss.ko
+  HIDDEN:=1
+  AUTOLOAD:=$(call AutoProbe,intel-lpss)
+endef
+
+$(eval $(call KernelPackage,intel-lpss))
+
+
+define KernelPackage/intel-lpss-acpi
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=Intel LPSS ACPI
+  DEPENDS:=+kmod-intel-lpss
+  KCONFIG:=CONFIG_INTEL_LPSS_ACPI
+  FILES:=$(LINUX_DIR)/drivers/mfd/intel-lpss-acpi.ko
+  AUTOLOAD:=$(call AutoProbe,intel-lpss-acpi)
+endef
+
+define KernelPackage/intel-lpss-acpi/description
+Kernel module to support Intel Low Power Subsystem (LPSS) devices such as
+I2C, SPI and HS-UART starting from Intel Sunrisepoint (Intel Skylake
+PCH) in ACPI mode.
+
+The actual hardware driver (eg. kmod-i2c-designware-platform) is still
+needed in addition to this package.
+endef
+
+$(eval $(call KernelPackage,intel-lpss-acpi))
+
+
+define KernelPackage/intel-lpss-pci
+  SUBMENU:=$(OTHER_MENU)
+  TITLE:=Intel LPSS PCI
+  DEPENDS:=+kmod-intel-lpss
+  KCONFIG:=CONFIG_INTEL_LPSS_PCI
+  FILES:=$(LINUX_DIR)/drivers/mfd/intel-lpss-pci.ko
+  AUTOLOAD:=$(call AutoProbe,intel-lpss-pci)
+endef
+
+define KernelPackage/intel-lpss-pci/description
+Kernel module to support Intel Low Power Subsystem (LPSS) devices such as
+I2C, SPI and HS-UART starting from Intel Sunrisepoint (Intel Skylake
+PCH) in PCI mode.
+
+The actual hardware driver (eg. kmod-i2c-designware-platform) is still
+needed in addition to this package.
+endef
+
+$(eval $(call KernelPackage,intel-lpss-pci))
+
+
 define KernelPackage/it87-wdt
   SUBMENU:=$(OTHER_MENU)
   TITLE:=ITE IT87 Watchdog Timer

commit 57736d4c79ad5db7cd144497b110aaf10be6f459

x86: modules: fix wrong Kconfig symbol
It's CONFIG_MFD_INTEL_LPSS, not CONFIG_INTEL_LPSS.

Fixes: b529d46405 ("x86: modules: add packages for Intel LPSS drivers")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/x86/modules.mk
@@ -104,7 +104,7 @@ define KernelPackage/intel-lpss
   SUBMENU:=$(OTHER_MENU)
   TITLE:=Intel LPSS common
   DEPENDS:=@TARGET_x86
-  KCONFIG:=CONFIG_INTEL_LPSS
+  KCONFIG:=CONFIG_MFD_INTEL_LPSS
   FILES:=$(LINUX_DIR)/drivers/mfd/intel-lpss.ko
   HIDDEN:=1
   AUTOLOAD:=$(call AutoProbe,intel-lpss)
@@ -117,7 +117,7 @@ define KernelPackage/intel-lpss-acpi
   SUBMENU:=$(OTHER_MENU)
   TITLE:=Intel LPSS ACPI
   DEPENDS:=+kmod-intel-lpss
-  KCONFIG:=CONFIG_INTEL_LPSS_ACPI
+  KCONFIG:=CONFIG_MFD_INTEL_LPSS_ACPI
   FILES:=$(LINUX_DIR)/drivers/mfd/intel-lpss-acpi.ko
   AUTOLOAD:=$(call AutoProbe,intel-lpss-acpi)
 endef
@@ -137,8 +137,8 @@ $(eval $(call KernelPackage,intel-lpss-acpi))
 define KernelPackage/intel-lpss-pci
   SUBMENU:=$(OTHER_MENU)
   TITLE:=Intel LPSS PCI
-  DEPENDS:=+kmod-intel-lpss
-  KCONFIG:=CONFIG_INTEL_LPSS_PCI
+  DEPENDS:=+kmod-intel-lpss @PCI_SUPPORT
+  KCONFIG:=CONFIG_MFD_INTEL_LPSS_PCI
   FILES:=$(LINUX_DIR)/drivers/mfd/intel-lpss-pci.ko
   AUTOLOAD:=$(call AutoProbe,intel-lpss-pci)
 endef