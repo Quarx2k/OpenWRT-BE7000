janh/openwrt

realtek: implement polling for hardware counters
Maintain 64 bit counters by polling the hardware counters and adding up
the differences. Polling needs to happen just often enough to catch
every single overflow.

As we now have non-overflowing counters now, we can safely calculate
composite counters without getting weird results on overflow. Use this
to follow RFC 3635 more accurately by mapping the hardware counters to
the proper counters, while taking into account hardware quirks as best
as possible.

Signed-off-by: Jan Hoffmann <jan@3e8.eu>

target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/rtl838x.h
@@ -639,6 +639,51 @@ enum pbvlan_mode {
 	PBVLAN_MODE_ALL_PKT,
 };
 
+struct rtl83xx_counter {
+	uint64_t val;
+	uint32_t last;
+};
+
+struct rtl83xx_counter_state {
+	spinlock_t lock;
+	ktime_t last_update;
+
+	struct rtl83xx_counter symbol_errors;
+
+	struct rtl83xx_counter if_in_octets;
+	struct rtl83xx_counter if_out_octets;
+	struct rtl83xx_counter if_in_ucast_pkts;
+	struct rtl83xx_counter if_in_mcast_pkts;
+	struct rtl83xx_counter if_in_bcast_pkts;
+	struct rtl83xx_counter if_out_ucast_pkts;
+	struct rtl83xx_counter if_out_mcast_pkts;
+	struct rtl83xx_counter if_out_bcast_pkts;
+	struct rtl83xx_counter if_out_discards;
+	struct rtl83xx_counter single_collisions;
+	struct rtl83xx_counter multiple_collisions;
+	struct rtl83xx_counter deferred_transmissions;
+	struct rtl83xx_counter late_collisions;
+	struct rtl83xx_counter excessive_collisions;
+	struct rtl83xx_counter crc_align_errors;
+	struct rtl83xx_counter rx_pkts_over_max_octets;
+
+	struct rtl83xx_counter unsupported_opcodes;
+
+	struct rtl83xx_counter rx_undersize_pkts;
+	struct rtl83xx_counter rx_oversize_pkts;
+	struct rtl83xx_counter rx_fragments;
+	struct rtl83xx_counter rx_jabbers;
+
+	struct rtl83xx_counter tx_pkts[ETHTOOL_RMON_HIST_MAX];
+	struct rtl83xx_counter rx_pkts[ETHTOOL_RMON_HIST_MAX];
+
+	struct rtl83xx_counter drop_events;
+	struct rtl83xx_counter collisions;
+
+	struct rtl83xx_counter rx_pause_frames;
+	struct rtl83xx_counter tx_pause_frames;
+};
+
 struct rtl838x_port {
 	bool enable;
 	u64 pm;
@@ -651,6 +696,7 @@ struct rtl838x_port {
 	int sds_num;
 	int led_set;
 	int leds_on_this_port;
+	struct rtl83xx_counter_state counters;
 	const struct dsa_port *dp;
 };
 
@@ -1116,6 +1162,7 @@ struct rtl838x_switch_priv {
 	struct rtl838x_l3_intf *interfaces[MAX_INTERFACES];
 	u16 intf_mtus[MAX_INTF_MTUS];
 	int intf_mtu_count[MAX_INTF_MTUS];
+	struct delayed_work counters_work;
 };
 
 void rtl838x_dbgfs_init(struct rtl838x_switch_priv *priv);

target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/rtl83xx.h
@@ -58,6 +58,7 @@ struct rtl83xx_mib_desc {
 	struct rtl83xx_mib_item late_collisions;
 	struct rtl83xx_mib_item excessive_collisions;
 	struct rtl83xx_mib_item crc_align_errors;
+	struct rtl83xx_mib_item rx_pkts_over_max_octets;
 
 	struct rtl83xx_mib_item unsupported_opcodes;
 

Quarx2k/OpenWRT-BE7000

Link: https://github.com/openwrt/openwrt/pull/18415
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/rtl838x.h
@@ -639,6 +639,51 @@ enum pbvlan_mode {
 	PBVLAN_MODE_ALL_PKT,
 };
 
+struct rtldsa_counter {
+	uint64_t val;
+	uint32_t last;
+};
+
+struct rtldsa_counter_state {
+	spinlock_t lock;
+	ktime_t last_update;
+
+	struct rtldsa_counter symbol_errors;
+
+	struct rtldsa_counter if_in_octets;
+	struct rtldsa_counter if_out_octets;
+	struct rtldsa_counter if_in_ucast_pkts;
+	struct rtldsa_counter if_in_mcast_pkts;
+	struct rtldsa_counter if_in_bcast_pkts;
+	struct rtldsa_counter if_out_ucast_pkts;
+	struct rtldsa_counter if_out_mcast_pkts;
+	struct rtldsa_counter if_out_bcast_pkts;
+	struct rtldsa_counter if_out_discards;
+	struct rtldsa_counter single_collisions;
+	struct rtldsa_counter multiple_collisions;
+	struct rtldsa_counter deferred_transmissions;
+	struct rtldsa_counter late_collisions;
+	struct rtldsa_counter excessive_collisions;
+	struct rtldsa_counter crc_align_errors;
+	struct rtldsa_counter rx_pkts_over_max_octets;
+
+	struct rtldsa_counter unsupported_opcodes;
+
+	struct rtldsa_counter rx_undersize_pkts;
+	struct rtldsa_counter rx_oversize_pkts;
+	struct rtldsa_counter rx_fragments;
+	struct rtldsa_counter rx_jabbers;
+
+	struct rtldsa_counter tx_pkts[ETHTOOL_RMON_HIST_MAX];
+	struct rtldsa_counter rx_pkts[ETHTOOL_RMON_HIST_MAX];
+
+	struct rtldsa_counter drop_events;
+	struct rtldsa_counter collisions;
+
+	struct rtldsa_counter rx_pause_frames;
+	struct rtldsa_counter tx_pause_frames;
+};
+
 struct rtl838x_port {
 	bool enable;
 	u64 pm;
@@ -651,6 +696,7 @@ struct rtl838x_port {
 	int sds_num;
 	int led_set;
 	int leds_on_this_port;
+	struct rtldsa_counter_state counters;
 	const struct dsa_port *dp;
 };
 
@@ -1116,6 +1162,7 @@ struct rtl838x_switch_priv {
 	struct rtl838x_l3_intf *interfaces[MAX_INTERFACES];
 	u16 intf_mtus[MAX_INTF_MTUS];
 	int intf_mtu_count[MAX_INTF_MTUS];
+	struct delayed_work counters_work;
 };
 
 void rtl838x_dbgfs_init(struct rtl838x_switch_priv *priv);

target/linux/realtek/files-6.12/drivers/net/dsa/rtl83xx/rtl83xx.h
@@ -58,6 +58,7 @@ struct rtldsa_mib_desc {
 	struct rtldsa_mib_item late_collisions;
 	struct rtldsa_mib_item excessive_collisions;
 	struct rtldsa_mib_item crc_align_errors;
+	struct rtldsa_mib_item rx_pkts_over_max_octets;
 
 	struct rtldsa_mib_item unsupported_opcodes;
 