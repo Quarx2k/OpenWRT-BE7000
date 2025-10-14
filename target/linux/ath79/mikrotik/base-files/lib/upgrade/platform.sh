Quarx2k/OpenWRT-BE7000

commit 327a00a1dfe50cef69b879b3fbff912cfb791429

ath79: mikrotik: check RouterBOOT version matching sysupgrade image
Check if the uploaded image matches the version of RouterBOOT before
proceeding with sysupgrade on MikroTik devices with NOR flash.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/ath79/mikrotik/base-files/lib/upgrade/platform.sh
@@ -3,7 +3,48 @@
 PART_NAME=firmware
 
 REQUIRE_IMAGE_METADATA=1
+
+platform_check_image_mikrotik_nor() {
+	local bootfwver bootfwmajor
+	local bootentry="$(dd bs=10 skip=1 count=1 if="$1" 2>/dev/null | xargs -0)"
+
+	read -r bootfwver < /sys/firmware/mikrotik/hard_config/booter_version
+	bootfwmajor="${bootfwver%%.*}"
+
+	if [ "$((bootfwmajor))" = 0 ]; then
+		v "invalid RouterBOOT version"
+		return 1
+	elif [ "$bootfwmajor" -le 6 ] && [ "$bootentry" != "kernel" ]; then
+		v "RouterBOOT 6 and earlier requires ELF-in-YAFFS image"
+		return 1
+	elif [ "$bootfwmajor" -ge 7 ] && [ "$bootentry" != "bootimage" ]; then
+		v "RouterBOOT 7 and later requires NPK-in-YAFFS image"
+		return 1
+	fi
+
+	return 0
+}
+
 platform_check_image() {
+	case "$board" in
+	mikrotik,routerboard-2011uias-2hnd|\
+	mikrotik,routerboard-493g|\
+	mikrotik,routerboard-911g-5hpacd|\
+	mikrotik,routerboard-911g-xhpnd|\
+	mikrotik,routerboard-912uag-2hpnd|\
+	mikrotik,routerboard-921gs-5hpacd-15s|\
+	mikrotik,routerboard-922uags-5hpacd|\
+	mikrotik,routerboard-951g-2hnd|\
+	mikrotik,routerboard-951ui-2hnd|\
+	mikrotik,routerboard-sxt-5nd-r2)
+		return 0
+		;;
+	*)
+		platform_check_image_mikrotik_nor "$1"
+		return $?
+		;;
+	esac
+
 	return 0
 }
 

commit d91d4fb5debfb289c4f492b346c47d34c14f3f5e

ath79: fix broken MikroTik upgrade
Fix a regression introduced by a recent commit.
It looks like a copy/paste error.

Add the missing line which defines the 'board' val
as it does not exist otherwise in the case check.

This fixes sysupgrade on ath79 MikroTik non-NOR boards.

Fixes: 327a00a1df ("ath79: mikrotik: check RouterBOOT version matching sysupgrade image")
Signed-off-by: Koen Vandeputte <koen.vandeputte@citymesh.com>

target/linux/ath79/mikrotik/base-files/lib/upgrade/platform.sh
@@ -26,6 +26,8 @@ platform_check_image_mikrotik_nor() {
 }
 
 platform_check_image() {
+	local board=$(board_name)
+
 	case "$board" in
 	mikrotik,routerboard-2011uias-2hnd|\
 	mikrotik,routerboard-493g|\