Quarx2k/OpenWRT-BE7000

commit c283629dd6917dfd4a347182cc847c3a1abf9fed

mediatek: build image for MT7987 RFB
Import and clean DT and DT-overlay files from MediaTek's SDK to build
an image with various DT-overlays for the MT7987 reference board.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/filogic.mk
@@ -24,6 +24,14 @@ define Build/mt7986-bl31-uboot
 	cat $(STAGING_DIR_IMAGE)/mt7986_$1-u-boot.fip >> $@
 endef
 
+define Build/mt7987-bl2
+	cat $(STAGING_DIR_IMAGE)/mt7987-$1-bl2.img >> $@
+endef
+
+define Build/mt7987-bl31-uboot
+	cat $(STAGING_DIR_IMAGE)/mt7987_$1-u-boot.fip >> $@
+endef
+
 define Build/mt7988-bl2
 	cat $(STAGING_DIR_IMAGE)/mt7988-$1-bl2.img >> $@
 endef
@@ -1611,6 +1619,57 @@ define Device/mediatek_mt7986b-rfb
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += mediatek_mt7986b-rfb
+
+define Device/mediatek_mt7987a-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := MT7987A rfb
+  DEVICE_DTS := mt7987a-rfb
+  DEVICE_DTS_OVERLAY:= \
+	mt7987a-rfb-spim-nand \
+	mt7987a-rfb-spim-nor \
+	mt7987a-rfb-emmc \
+	mt7987a-rfb-sd \
+	mt7987a-rfb-eth0-an8801sb \
+	mt7987a-rfb-eth0-an8855 \
+	mt7987a-rfb-eth0-e2p5g \
+	mt7987a-rfb-eth0-mt7531 \
+	mt7987a-rfb-eth1-i2p5g \
+	mt7987a-rfb-eth2-an8801sb \
+	mt7987a-rfb-eth2-e2p5g \
+	mt7987a-rfb-eth2-sfp \
+	mt7987a-rfb-eth2-usb
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_DTC_FLAGS := --pad 4096
+  DEVICE_DTS_LOADADDR := 0x4ff00000
+  DEVICE_PACKAGES := mt798x-2p5g-phy-firmware-internal kmod-sfp blkid
+  KERNEL_LOADADDR := 0x40000000
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+        fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  IMAGES := sysupgrade.itb
+  KERNEL_INITRAMFS_SUFFIX := .itb
+  KERNEL_IN_UBI := 1
+  IMAGE_SIZE := $$(shell expr 64 + $$(CONFIG_TARGET_ROOTFS_PARTSIZE))m
+  IMAGES := sysupgrade.itb
+  IMAGE/sysupgrade.itb := append-kernel | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | pad-rootfs | append-metadata
+  ARTIFACTS := \
+	snand-preloader.bin \
+	snand-bl31-uboot.fip \
+	sdcard.img.gz
+  ARTIFACT/snand-preloader.bin	:= mt7987-bl2 spim-nand0-ubi-comb
+  ARTIFACT/snand-bl31-uboot.fip	:= mt7987-bl31-uboot rfb-spim-nand
+  ARTIFACT/sdcard.img.gz	:= mt798x-gpt sdmmc |\
+				   pad-to 17k | mt7987-bl2 sdmmc-comb |\
+				   pad-to 6656k | mt7987-bl31-uboot rfb-sd |\
+				$(if $(CONFIG_TARGET_ROOTFS_INITRAMFS),\
+				  pad-to 12M | append-image-stage initramfs.itb | check-size 44m |\
+				) \
+				$(if $(CONFIG_TARGET_ROOTFS_SQUASHFS),\
+				  pad-to 64M | append-image squashfs-sysupgrade.itb | check-size |\
+				) \
+				  gzip
+endef
+TARGET_DEVICES += mediatek_mt7987a-rfb
 
 define Device/mediatek_mt7988a-rfb
   DEVICE_VENDOR := MediaTek

target/linux/mediatek/dts/mt7987a-rfb-emmc.dtso
@@ -1,0 +1,101 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+
+/dts-v1/;
+/plugin/;
+
+/ {
+	fragment@0 {
+		target-path = "/";
+		__overlay__ {
+			reg_3p3v: regulator-3p3v {
+				compatible = "regulator-fixed";
+				regulator-name = "fixed-3.3V";
+				regulator-min-microvolt = <3300000>;
+				regulator-max-microvolt = <3300000>;
+				regulator-boot-on;
+				regulator-always-on;
+			};
+		};
+	};
+
+	fragment@1 {
+		target-path = "/soc/spi@11007800";
+		__overlay__ {
+			status = "disabled";
+		};
+	};
+
+	fragment@2 {
+		target-path = "/soc/mmc@11230000";
+		__overlay__ {
+			pinctrl-names = "default", "state_uhs";
+			pinctrl-0 = <&mmc_pins_default>;
+			pinctrl-1 = <&mmc_pins_uhs>;
+			bus-width = <8>;
+			max-frequency = <48000000>;
+			cap-mmc-highspeed;
+			vmmc-supply = <&reg_3p3v>;
+			non-removable;
+			status = "okay";
+			#address-cells = <1>;
+			#size-cells = <0>;
+
+			card@0 {
+				compatible = "mmc-card";
+				reg = <0>;
+
+				block {
+					compatible = "block-device";
+					partitions {
+						block-partition-env {
+							partname = "ubootenv";
+							nvmem-layout {
+								compatible = "u-boot,env";
+							};
+						};
+
+						block-partition-factory {
+							partname = "factory";
+
+							nvmem-layout {
+								compatible = "fixed-layout";
+								#address-cells = <1>;
+								#size-cells = <1>;
+
+								eeprom_factory_0: eeprom@0 {
+									reg = <0x0 0x1e00>;
+								};
+							};
+						};
+
+						emmc_rootfs: block-partition-production {
+							partname = "production";
+						};
+					};
+				};
+			};
+		};
+	};
+
+	fragment@3 {
+		target-path = "/chosen";
+		__overlay__ {
+			rootdisk-emmc = <&emmc_rootfs>;
+		};
+	};
+
+	fragment@4 {
+		target = <&pcie0>;
+		__overlay__ {
+			slot0: pcie@0,0 {
+				reg = <0x0000 0 0 0 0>;
+				mt7996@0,0 {
+					reg = <0x0000 0 0 0 0>;
+					nvmem-cells = <&eeprom_factory_0>;
+					nvmem-cell-names = "eeprom";
+				};
+			};
+		};
+	};
+};
+

target/linux/mediatek/dts/mt7987a-rfb-eth0-an8801sb.dtso
@@ -1,0 +1,32 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+
+/dts-v1/;
+/plugin/;
+
+#include <dt-bindings/gpio/gpio.h>
+
+/ {
+	fragment@0 {
+		target = <&gmac0>;
+		__overlay__ {
+			phy-handle = <&phy31>;
+			phy-mode = "sgmii";
+			status = "okay";
+		};
+	};
+
+	fragment@1 {
+		target-path = "/soc_netsys/ethernet@15100000/mdio-bus";
+		__overlay__ {
+			phy31: phy@31 {
+				compatible = "ethernet-phy-idc0ff.0421";
+				reg = <31>;
+				reset-gpios = <&pio 48 GPIO_ACTIVE_LOW>;
+				reset-assert-us = <10000>;
+				reset-deassert-us = <10000>;
+				eee-broken-100tx;
+				eee-broken-1000t;
+			};
+		};
+	};
+};

target/linux/mediatek/dts/mt7987a-rfb-eth0-an8855.dtso
@@ -1,0 +1,204 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+
+/dts-v1/;
+/plugin/;
+
+#include <dt-bindings/gpio/gpio.h>
+
+/ {
+	fragment@0 {
+		target = <&gmac0>;
+		__overlay__ {
+			phy-mode = "2500base-x";
+			status = "okay";
+			fixed-link {
+				speed = <2500>;
+				full-duplex;
+				pause;
+			};
+		};
+	};
+
+	fragment@1 {
+		target-path = "/soc_netsys/ethernet@15100000/mdio-bus";
+		__overlay__ {
+			mfd: mfd@1 {
+				compatible = "airoha,an8855-mfd";
+				reg = <1>;
+				status = "okay";
+
+				efuse {
+					compatible = "airoha,an8855-efuse";
+					#nvmem-cell-cells = <0>;
+
+					nvmem-layout {
+						compatible = "fixed-layout";
+						#address-cells = <1>;
+						#size-cells = <1>;
+
+						shift_sel_port0_tx_a: shift-sel-port0-tx-a@c {
+							reg = <0xc 0x4>;
+						};
+
+						shift_sel_port0_tx_b: shift-sel-port0-tx-b@10 {
+							reg = <0x10 0x4>;
+						};
+
+						shift_sel_port0_tx_c: shift-sel-port0-tx-c@14 {
+							reg = <0x14 0x4>;
+						};
+
+						shift_sel_port0_tx_d: shift-sel-port0-tx-d@18 {
+							reg = <0x18 0x4>;
+						};
+
+						shift_sel_port1_tx_a: shift-sel-port1-tx-a@1c {
+							reg = <0x1c 0x4>;
+						};
+
+						shift_sel_port1_tx_b: shift-sel-port1-tx-b@20 {
+							reg = <0x20 0x4>;
+						};
+
+						shift_sel_port1_tx_c: shift-sel-port1-tx-c@24 {
+							reg = <0x24 0x4>;
+						};
+
+						shift_sel_port1_tx_d: shift-sel-port1-tx-d@28 {
+							reg = <0x28 0x4>;
+						};
+
+						shift_sel_port2_tx_a: shift-sel-port2-tx-a@2c {
+							reg = <0x2c 0x4>;
+						};
+
+						shift_sel_port2_tx_b: shift-sel-port2-tx-b@30 {
+							reg = <0x30 0x4>;
+						};
+
+						shift_sel_port2_tx_c: shift-sel-port2-tx-c@34 {
+							reg = <0x34 0x4>;
+						};
+
+						shift_sel_port2_tx_d: shift-sel-port2-tx-d@38 {
+							reg = <0x38 0x4>;
+						};
+
+						shift_sel_port3_tx_a: shift-sel-port3-tx-a@4c {
+							reg = <0x4c 0x4>;
+						};
+
+						shift_sel_port3_tx_b: shift-sel-port3-tx-b@50 {
+							reg = <0x50 0x4>;
+						};
+
+						shift_sel_port3_tx_c: shift-sel-port3-tx-c@54 {
+							reg = <0x54 0x4>;
+						};
+
+						shift_sel_port3_tx_d: shift-sel-port3-tx-d@58 {
+							reg = <0x58 0x4>;
+						};
+					};
+				};
+
+				ethernet-switch {
+					compatible = "airoha,an8855-switch";
+					reset-gpios = <&pio 42 GPIO_ACTIVE_HIGH>;
+					airoha,ext-surge;
+
+					ports {
+						#address-cells = <1>;
+						#size-cells = <0>;
+
+						port@0 {
+							reg = <0>;
+							label = "lan0";
+							phy-mode = "internal";
+							phy-handle = <&internal_phy1>;
+						};
+
+						port@1 {
+							reg = <1>;
+							label = "lan1";
+							phy-mode = "internal";
+							phy-handle = <&internal_phy2>;
+						};
+
+						port@2 {
+							reg = <2>;
+							label = "lan2";
+							phy-mode = "internal";
+							phy-handle = <&internal_phy3>;
+						};
+
+						port@3 {
+							reg = <3>;
+							label = "lan3";
+							phy-mode = "internal";
+							phy-handle = <&internal_phy4>;
+						};
+
+						port@5 {
+							reg = <5>;
+							ethernet = <&gmac0>;
+							phy-mode = "2500base-x";
+
+							fixed-link {
+								speed = <2500>;
+								full-duplex;
+								pause;
+							};
+						};
+					};
+				};
+
+				mdio {
+					compatible = "airoha,an8855-mdio";
+					#address-cells = <1>;
+					#size-cells = <0>;
+
+					internal_phy1: phy@1 {
+						reg = <1>;
+
+						nvmem-cells = <&shift_sel_port0_tx_a>,
+								<&shift_sel_port0_tx_b>,
+								<&shift_sel_port0_tx_c>,
+								<&shift_sel_port0_tx_d>;
+						nvmem-cell-names = "tx_a", "tx_b", "tx_c", "tx_d";
+					};
+
+					internal_phy2: phy@2 {
+						reg = <2>;
+
+						nvmem-cells = <&shift_sel_port1_tx_a>,
+								<&shift_sel_port1_tx_b>,
+								<&shift_sel_port1_tx_c>,
+								<&shift_sel_port1_tx_d>;
+						nvmem-cell-names = "tx_a", "tx_b", "tx_c", "tx_d";
+					};
+
+					internal_phy3: phy@3 {
+						reg = <3>;
+
+						nvmem-cells = <&shift_sel_port2_tx_a>,
+								<&shift_sel_port2_tx_b>,
+								<&shift_sel_port2_tx_c>,
+								<&shift_sel_port2_tx_d>;
+						nvmem-cell-names = "tx_a", "tx_b", "tx_c", "tx_d";
+					};
+
+					internal_phy4: phy@4 {
+						reg = <4>;
+
+						nvmem-cells = <&shift_sel_port3_tx_a>,
+								<&shift_sel_port3_tx_b>,
+								<&shift_sel_port3_tx_c>,
+								<&shift_sel_port3_tx_d>;
+						nvmem-cell-names = "tx_a", "tx_b", "tx_c", "tx_d";
+					};
+				};
+			};
+		};
+	};
+};

target/linux/mediatek/dts/mt7987a-rfb-eth0-e2p5g.dtso
@@ -1,0 +1,32 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+
+/dts-v1/;
+/plugin/;
+
+#include <dt-bindings/gpio/gpio.h>
+
+/ {
+	fragment@0 {
+		target = <&gmac0>;
+		__overlay__ {
+			phy-handle = <&phy12>;
+			phy-mode = "2500base-x";
+			status = "okay";
+		};
+	};
+
+	fragment@1 {
+		target-path = "/soc_netsys/ethernet@15100000/mdio-bus";
+		__overlay__ {
+			phy12: phy@12 {
+				compatible = "ethernet-phy-id03a2.a411";
+				reg = <12>;
+				reset-gpios = <&pio 49 GPIO_ACTIVE_LOW>;
+				reset-assert-us = <10000>;
+				reset-deassert-us = <10000>;
+				full-duplex;
+				pause;
+			};
+		};
+	};
+};

target/linux/mediatek/dts/mt7987a-rfb-eth0-mt7531.dtso
@@ -1,0 +1,90 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+
+/dts-v1/;
+/plugin/;
+
+/ {
+	fragment@0 {
+		target = <&gmac0>;
+		__overlay__ {
+			phy-mode = "2500base-x";
+			status = "okay";
+			fixed-link {
+				speed = <2500>;
+				full-duplex;
+				pause;
+			};
+		};
+	};
+
+	fragment@1 {
+		target-path = "/soc_netsys/ethernet@15100000/mdio-bus";
+		__overlay__ {
+			phy12: phy@12 {
+				compatible = "ethernet-phy-id03a2.a411";
+				reg = <12>;
+				reset-gpios = <&pio 49 1>;
+				reset-assert-us = <10000>;
+				reset-deassert-us = <10000>;
+				phy-mode = "2500base-x";
+				full-duplex;
+				pause;
+				airoha,pnswap-rx;
+			};
+
+			switch31: switch@31 {
+				compatible = "mediatek,mt7531";
+				reg = <31>;
+				reset-gpios = <&pio 42 0>;
+				status = "okay";
+
+				ports {
+					#address-cells = <1>;
+					#size-cells = <0>;
+
+					port@0 {
+						reg = <0>;
+						label = "lan0";
+					};
+
+					port@1 {
+						reg = <1>;
+						label = "lan1";
+					};
+
+					port@2 {
+						reg = <2>;
+						label = "lan2";
+					};
+
+					port@3 {
+						reg = <3>;
+						label = "lan3";
+					};
+
+					port@5 {
+						reg = <5>;
+						label = "lan5";
+						phy-mode = "2500base-x";
+						phy-handle = <&phy12>;
+					};
+
+					port@6 {
+						reg = <6>;
+						label = "cpu";
+						ethernet = <&gmac0>;
+						phy-mode = "2500base-x";
+
+						fixed-link {
+							speed = <2500>;
+							full-duplex;
+							pause;
+						};
+					};
+				};
+
+			};
+		};
+	};
+};
+

target/linux/mediatek/dts/mt7987a-rfb-eth1-i2p5g.dtso
@@ -1,0 +1,30 @@
+// SPDX-License-Identifier: (GPL-2.0 OR MIT)
+
+/dts-v1/;
+/plugin/;
+
+/ {
+	fragment@0 {
+		target = <&gmac1>;
+		__overlay__ {
+			phy-mode = "internal";
+			phy-handle = <&phy15>;
+			status = "okay";
+		};
+	};
+
+	fragment@1 {
+		target-path = "/soc_netsys/ethernet@15100000/mdio-bus";
+		__overlay__ {
+			/* built-in 2.5G Ethernet PHY */
+			phy15: phy@15 {
+				pinctrl-names = "i2p5gbe-led";
+				pinctrl-0 = <&i2p5gbe_led0_pins>;
+				compatible = "ethernet-phy-ieee802.3-c45";
+				reg = <15>;
+				phy-mode = "internal";
+			};
+		};
+	};
+};
+

commit e9064ff2119809a026dc0b1e89f9dda23f7664c3

mediatek: remove non-existent package mt798x-2p5g-phy-firmware-internal
Also the MT7987 RFB accidentally already set the not-yet-existing
mt798x-2p5g-phy-firmware-internal package as one of the DEVICE_PACKAGES.
This currently breaks the build, so remove it for now. Also remove stray
'blkid' package from DEVICE_PACKAGES which was accidentally copied from
MediaTek's SDK.

Fixes: c283629dd6 ("mediatek: build image for MT7987 RFB")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

target/linux/mediatek/image/filogic.mk
@@ -1702,7 +1702,7 @@ define Device/mediatek_mt7987a-rfb
   DEVICE_DTS_DIR := ../dts
   DEVICE_DTC_FLAGS := --pad 4096
   DEVICE_DTS_LOADADDR := 0x4ff00000
-  DEVICE_PACKAGES := mt798x-2p5g-phy-firmware-internal kmod-sfp blkid
+  DEVICE_PACKAGES := kmod-sfp
   KERNEL_LOADADDR := 0x40000000
   KERNEL := kernel-bin | gzip
   KERNEL_INITRAMFS := kernel-bin | lzma | \