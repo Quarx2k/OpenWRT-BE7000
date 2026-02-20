Quarx2k/OpenWRT-BE7000

commit dd3f4889bf84d670657e1e9574d0d77cab1edac6

qualcommbe: move Device DTS to dedicated DTS directory
Align the qualcommbe target to the pattern already used on other devices where
the device DTS are placed in a dedicated directory separate from the files
directory.

This, while trying to enforce a common pattern for every target, also permits to
do modification to device DTS without having to trigger a recompilation of the
entire kernel (as the files directory is not touched)

Link: https://github.com/openwrt/openwrt/pull/22038
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

target/linux/qualcommbe/image/ipq95xx.mk
@@ -1,3 +1,5 @@
+DTS_DIR := $(DTS_DIR)/qcom
+
 define Device/8devices_kiwi-dvk
 	$(call Device/FitImage)
 	$(call Device/EmmcImage)

target/linux/qualcommbe/image/Makefile
@@ -6,7 +6,7 @@ define Device/Default
 	KERNEL_LOADADDR := 0x41000000
 	DEVICE_DTS = $$(SOC)-$(lastword $(subst _, ,$(1)))
 	DEVICE_DTS_CONFIG := config@1
-	DEVICE_DTS_DIR := $(DTS_DIR)/qcom
+	DEVICE_DTS_DIR := ../dts
 	IMAGES := sysupgrade.bin
 	IMAGE/sysupgrade.bin = sysupgrade-tar | append-metadata
 	IMAGE/sysupgrade.bin/squashfs :=

commit 738dab22cd39e196abf5c1217a17ddb89d1f6599

qualcommbe: fix rdp433 build after DTS directory move
Commit dd3f4889bf ("qualcommbe: move Device DTS to dedicated DTS
directory") introduced a dedicated DTS directory and set DEVICE_DTS_DIR
to ../dts as default. This broke the rdp433 device build since its DTS
resides in the kernel tree (applied via patches), not in the target dts
directory.

Fix this by overriding DEVICE_DTS_DIR for rdp433 to point to the kernel
DTS directory.

Fixes: dd3f4889bf ("qualcommbe: move Device DTS to dedicated DTS directory")
Signed-off-by: Ahmed Naseef <naseefkm@gmail.com>
Link: https://github.com/openwrt/openwrt/pull/22096
Signed-off-by: Robert Marko <robimarko@gmail.com>

target/linux/qualcommbe/image/ipq95xx.mk
@@ -19,6 +19,7 @@ define Device/qcom_rdp433
 	DEVICE_VARIANT := AP-AL02-C4
 	BOARD_NAME := ap-al02.1-c4
 	DEVICE_DTS_CONFIG := config@rdp433
+	DEVICE_DTS_DIR := $(DTS_DIR)
 	SOC := ipq9574
 	KERNEL_INSTALL := 1
 	KERNEL_SIZE := 6096k