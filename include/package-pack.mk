openwrt/openwrt

build: add additional apk tags
This commit adds additional information to the apk database. This new tags
are required for SBOM generation.

Signed-off-by: Florian Eckert <fe@dev.tdt.de>

include/package-pack.mk
@@ -328,6 +328,22 @@ endif
       APK_SCRIPTS_$(1)+=--script "post-deinstall:$$(ADIR_$(1))/postrm"
     endif
 
+ifeq ($(CONFIG_USE_APK),)
+    APK_TAGS_$(1):=
+
+    ifneq ($$(ABIV_$(1)),)
+      APK_TAGS_$(1)+=openwrt:abiversion=$$(ABIV_$(1))
+    endif
+
+    ifneq ($(SECTION),)
+      APK_TAGS_$(1)+=openwrt:section=$(SECTION)
+    endif
+
+    ifneq ($(PKG_CPE_ID),)
+      APK_TAGS_$(1)+=openwrt:cpe=$(PKG_CPE_ID)
+    endif
+endif
+
     TARGET_VARIANT:=$$(if $(ALL_VARIANTS),$$(if $$(VARIANT),$$(filter-out *,$$(VARIANT)),$(firstword $(ALL_VARIANTS))))
     ifeq ($(BUILD_VARIANT),$$(if $$(TARGET_VARIANT),$$(TARGET_VARIANT),$(BUILD_VARIANT)))
     do_install=
@@ -593,7 +609,7 @@ else
 	$(FAKEROOT) $(STAGING_DIR_HOST)/bin/apk mkpkg \
 	  --info "name:$(1)$$(ABIV_$(1))" \
 	  --info "version:$(VERSION)" \
-	  $$(if $$(ABIV_$(1)),--info "tags:openwrt:abiversion=$$(ABIV_$(1))") \
+	  $$(if $$(APK_TAGS_$(1)),--info "tags:$$(APK_TAGS_$(1))") \
 	  --info "description:$$(call description_escape,$$(strip $$(Package/$(1)/description)))" \
 	  $(if $(findstring all,$(PKGARCH)),--info "arch:noarch",--info "arch:$(PKGARCH)") \
 	  --info "license:$(LICENSE)" \

Quarx2k/OpenWRT-BE7000

include/package-pack.mk
@@ -328,6 +328,22 @@ endif
       APK_SCRIPTS_$(1)+=--script "post-deinstall:$$(ADIR_$(1))/postrm"
     endif
 
+ifneq ($(CONFIG_USE_APK),)
+    APK_TAGS_$(1):=
+
+    ifneq ($$(ABIV_$(1)),)
+      APK_TAGS_$(1)+=openwrt:abiversion=$$(ABIV_$(1))
+    endif
+
+    ifneq ($(SECTION),)
+      APK_TAGS_$(1)+=openwrt:section=$(SECTION)
+    endif
+
+    ifneq ($(PKG_CPE_ID),)
+      APK_TAGS_$(1)+=openwrt:cpe=$(PKG_CPE_ID)
+    endif
+endif
+
     TARGET_VARIANT:=$$(if $(ALL_VARIANTS),$$(if $$(VARIANT),$$(filter-out *,$$(VARIANT)),$(firstword $(ALL_VARIANTS))))
     ifeq ($(BUILD_VARIANT),$$(if $$(TARGET_VARIANT),$$(TARGET_VARIANT),$(BUILD_VARIANT)))
     do_install=
@@ -593,7 +609,7 @@ else
 	$(FAKEROOT) $(STAGING_DIR_HOST)/bin/apk mkpkg \
 	  --info "name:$(1)$$(ABIV_$(1))" \
 	  --info "version:$(VERSION)" \
-	  $$(if $$(ABIV_$(1)),--info "tags:openwrt:abiversion=$$(ABIV_$(1))") \
+	  $$(if $$(APK_TAGS_$(1)),--info "tags:$$(APK_TAGS_$(1))") \
 	  --info "description:$$(call description_escape,$$(strip $$(Package/$(1)/description)))" \
 	  $(if $(findstring all,$(PKGARCH)),--info "arch:noarch",--info "arch:$(PKGARCH)") \
 	  --info "license:$(LICENSE)" \