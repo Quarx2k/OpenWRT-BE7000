LuisMitaHL/LooperWrt

commit 98cd55f6666330c0f029d3fc784a7a2a8976c7f6

wifi-scripts: add ucode based scripts
Add an ucode based re-implementation of the shell script based wifi code.

The new code is jsonschema driven. The code has been refactored into several
files making it easier to follow.

The new scripts are also way faster than the previous sh implementation.

The new code is currently opt-in via WIFI_SCRIPTS_UCODE and defaults to
EXPERIMENTAL.

Signed-off-by: John Crispin <john@phrozen.org>

package/network/config/wifi-scripts/files-ucode/usr/share/ucode/wifi/supplicant.uc
@@ -1,0 +1,237 @@
+'use strict';
+
+import { append, append_raw, append_vars, network_append, network_append_raw, network_append_vars,
+	 set_default, dump_network, flush_network } from 'wifi.common';
+import * as netifd from 'wifi.netifd';
+import * as iface from 'wifi.iface';
+import * as fs from 'fs';
+
+function set_fixed_freq(data, config) {
+	if (!data.frequency)
+		return;
+
+	set_default(config, 'fixed_freq', 1);
+	set_default(config, 'frequency', data.frequency);
+
+	if (data.htmode in [ 'VHT80', 'HE80' ])
+		set_default(config, 'max_oper_chwidth', 1);
+	else if (data.htmode in [ 'VHT160', 'HE160' ])
+		set_default(config, 'max_oper_chwidth', 2);
+	else if (data.htmode in [ 'VHT20', 'VHT40', 'HE20', 'HE40' ])
+		set_default(config, 'max_oper_chwidth', 0);
+	else
+		set_default(config, 'disable_vht', true);
+
+	if (data.htmode in [ 'NOHT' ])
+		set_default(config, 'disable_ht', true);
+	else if (data.htmode in [ 'HT20', 'VHT20', 'HE20' ])
+		set_default(config, 'disable_ht40', true);
+	else if (data.htmode in [ 'VHT40', 'VHT80', 'VHT160', 'HE40', 'HE80', 'HE160' ])
+		set_default(config, 'ht40', true);
+
+	if (wildcard(data.htmode, 'VHT*'))
+		set_default(config, 'vht', 1);
+}
+
+export function ratestr(rate) {
+	if (rate == null)
+		return rate;
+
+	let rem = (rate / 100) % 10;
+	rate = int(rate / 1000);
+	if (rem > 0)
+		rate += "." + rem;
+
+	return "" + rate;
+};
+
+export function ratelist(rates) {
+	if (length(rates) < 1)
+		return null;
+
+	return join(",", map(rates, (rate) => ratestr(rate)));
+};
+
+function setup_sta(data, config) {
+	iface.parse_encryption(config);
+
+	if (config.auth_type in [ 'sae', 'owe', 'eap2', 'eap192' ])
+		set_default(config, 'ieee80211w', 2);
+	else if (config.auth_type in [ 'psk-sae' ])
+		set_default(config, 'ieee80211w', 1);
+
+	set_default(config, 'ieee80211r', 0);
+	set_default(config, 'multi_ap', 0);
+	set_default(config, 'default_disabled', 0);
+
+//multiap_flag_file="${_config}.is_multiap"
+	config.scan_ssid = 1;
+
+	switch(config.mode) {
+	case 'sta':
+		set_default(config, 'multi_ap_backhaul_sta', config.multi_ap);
+		break;
+
+	case 'adhoc':
+		config.ap_scan = 2;
+		config.scan_ssid = 0;
+		network_append('mode', 1);
+		set_fixed_freq(data, config);
+		break;
+
+	case 'mesh':
+		config.ssid = config.mesh_id;
+		config.scan_ssid = null;
+		network_append('mode', 5);
+
+		set_fixed_freq(data, config);
+
+		if (config.encryption && config.encryption != 'none')
+			config.key_mgmt = 'SAE';
+
+		config.ieee80211w = null;
+		break;
+	}
+
+	if (config.mode != 'mesh' ) {
+		switch(config.wpa) {
+		case 1:
+			config.proto = 'WPA';
+			break;
+
+		case 2:
+			config.proto = 'RSN';
+			break;
+		}
+	}
+
+	switch(config.auth_type) {
+	case 'none':
+		break;
+
+	case 'owe':
+		iface.wpa_key_mgmt(config);
+		break;
+
+	case 'psk':
+	case 'psk2':
+	case 'sae':
+	case 'psk-sae':
+		if (config.mode != 'mesh')
+			iface.wpa_key_mgmt(config);
+
+		if (config.mode == 'mesh' || config.auth_type == 'sae')
+			config.sae_password = `"${config.key}"`;
+		else
+			config.psk = `"${config.key}"`;
+
+		break;
+
+	case 'eap':
+	case 'eap2':
+	case 'eap192':
+		iface.wpa_key_mgmt(config);
+		set_default(config, 'erp', config.fils);
+
+		if (config.ca_cert_usesystem && fs.stat('/etc/ssl/certs/ca-certificates.crt'))
+			config.ca_cert = '/etc/ssl/certs/ca-certificates.crt';
+
+		switch(config.eap_type) {
+		case 'fast':
+		case 'peap':
+		case 'ttls':
+			set_default(config, 'auth', 'MSCHAPV2');
+			if (config.auth == 'EAP-TLS') {
+				if (config.ca_cert2_usesystem && fs.stat('/etc/ssl/certs/ca-certificates.crt'))
+					config.ca_cert2 = '/etc/ssl/certs/ca-certificates.crt';
+			}
+			break;
+		}
+
+	}
+
+	if (config.wpa_pairwise == 'GCMP') {
+		config.pairwise = 'GCMP';
+		config.group = 'GCMP';
+	}
+
+	config.basic_rate = ratelist(config.basic_rate);
+	config.mcast_rate = ratestr(config.mcast_rate);
+	config.ssid = `"${config.ssid}"`;
+
+	network_append_vars(config, [
+		'scan_ssid', 'noscan', 'disabled', 'multi_ap_backhaul_sta',
+		'ocv', 'key_mgmt', 'psk', 'sae_password', 'pairwise', 'group', 'bssid',
+		'proto', 'mesh_fwding', 'mesh_rssi_threshold', 'frequency', 'fixed_freq',
+		'disable_ht', 'disable_ht40', 'disable_vht', 'vht', 'max_oper_chwidth',
+		'ht40', 'ssid', 'beacon_int', 'ieee80211w', 'basic_rate', 'mcast_rate',
+		'bssid_blacklist', 'bssid_whitelist', 'erp', 'ca_cert', 'identity',
+		'anonymous_identity', 'client_cert', 'private_key', 'private_key_passwd',
+		'subject_match', 'altsubject_match', 'domain_match', 'domain_suffix_match',
+		'ca_cert2', 'client_cert2', 'private_key2', 'private_key2_passwd', 'password'
+	]);
+}
+
+export function generate(config_list, data, interface) {
+	flush_network();
+
+	if (interface.bridge &&
+	    (interface.config.mode == 'adhoc' ||
+	     (interface.config.mode == 'sta' && !interface.config.wds && !interface.config.multi_ap))){
+		netifd.setup_failed('BRIDGE_NOT_ALLOWED');
+		return 1;
+	}
+
+	interface.config.country = data.config.country;
+	interface.config.beacon_int = data.config.beacon_int;
+
+	setup_sta(data.config, interface.config);
+
+	let file_name = `/var/run/wpa-supplicant-${interface.config.ifname}.conf`;
+	if (fs.stat(file_name))
+		fs.rename(file_name, file_name + '.prev');
+	dump_network(file_name);
+
+	let config = {
+		mode: interface.config.mode,
+		ctrl: '/var/run/wpa_supplicant',
+		iface: interface.config.ifname,
+		config: file_name,
+		'4addr': !!interface.config.wds,
+		powersave: false
+	};
+
+	if (!interface.config.default_macaddr)
+		config.macaddr = interface.config.macaddr;
+
+	if (interface.config.wds)
+		config.bridge = interface.bridge;
+
+	push(config_list, config);
+
+	return config;
+};
+
+export function setup(config, data) {
+	let ret = global.ubus.call('wpa_supplicant', 'config_set', {
+		phy: data.phy,
+		radio: data.config.radio,
+		config,
+		defer: true,
+		num_global_macaddr: data.config.num_global_macaddr,
+	});
+
+	if (ret)
+		netifd.add_process('/usr/sbin/wpa_supplicant', ret.pid, true, true);
+	else
+		netifd.setup_failed('SUPPLICANT_START_FAILED');
+};
+
+
+export function start(data) {
+	global.ubus.call('wpa_supplicant', 'config_set', {
+		phy: data.phy,
+		radio: data.config.radio,
+		num_global_macaddr: data.config.num_global_macaddr,
+	});
+};

package/network/config/wifi-scripts/Config.in
@@ -1,0 +1,3 @@
+config WIFI_SCRIPTS_UCODE
+	bool "Use new ucode based scripts"
+	default n

package/network/config/wifi-scripts/Makefile
@@ -13,15 +13,20 @@ PKG_RELEASE:=1
 PKG_LICENSE:=GPL-2.0
 
 PKG_MAINTAINER:=Felix Fietkau <nbd@nbd.name>
+PKG_CONFIG_DEPENDS:=CONFIG_WIFI_SCRIPTS_UCODE
 
 include $(INCLUDE_DIR)/package.mk
 
 define Package/wifi-scripts
   SECTION:=utils
   CATEGORY:=Base system
-  DEPENDS:=+netifd +ucode +ucode-mod-nl80211 +ucode-mod-rtnl +ucode-mod-ubus +ucode-mod-uci
+  DEPENDS:=+netifd +ucode +ucode-mod-nl80211 +ucode-mod-rtnl +ucode-mod-ubus +ucode-mod-uci +ucode-mod-digest
   TITLE:=Wi-Fi configuration scripts
   PKGARCH:=all
+endef
+
+define Package/wifi-scripts/config
+	source "$(SOURCE)/Config.in"
 endef
 
 define Package/wifi-scripts/description
@@ -40,6 +45,9 @@ endef
 define Package/wifi-scripts/install
 	$(INSTALL_DIR) $(1)
 	$(CP) ./files/* $(1)/
+ifeq ($(CONFIG_WIFI_SCRIPTS_UCODE),y)
+	$(CP) ./files-ucode/* $(1)/
+endif
 endef
 
 $(eval $(call BuildPackage,wifi-scripts))

package/network/config/wifi-scripts/files-ucode/lib/netifd/wireless/mac80211.sh
@@ -1,0 +1,347 @@
+#!/usr/bin/ucode
+
+'use strict';
+
+import { set_default, log } from 'wifi.common';
+import { validate, dump_options } from 'wifi.validate';
+import * as supplicant from 'wifi.supplicant';
+import * as hostapd from 'wifi.hostapd';
+import * as netifd from 'wifi.netifd';
+import * as iface from 'wifi.iface';
+import * as fs from 'fs';
+
+global.radio = ARGV[2];
+
+const mesh_param_list = [
+	"mesh_retry_timeout", "mesh_confirm_timeout", "mesh_holding_timeout", "mesh_max_peer_links",
+	"mesh_max_retries", "mesh_ttl", "mesh_element_ttl", "mesh_hwmp_max_preq_retries",
+	"mesh_path_refresh_time", "mesh_min_discovery_timeout", "mesh_hwmp_active_path_timeout",
+	"mesh_hwmp_preq_min_interval", "mesh_hwmp_net_diameter_traversal_time", "mesh_hwmp_rootmode",
+	"mesh_hwmp_rann_interval", "mesh_gate_announcements", "mesh_sync_offset_max_neighor",
+	"mesh_rssi_threshold", "mesh_hwmp_active_path_to_root_timeout", "mesh_hwmp_root_interval",
+	"mesh_hwmp_confirmation_interval", "mesh_awake_window", "mesh_plink_timeout",
+	"mesh_auto_open_plinks", "mesh_fwding", "mesh_power_mode"
+];
+
+function phy_suffix(radio, sep) {
+	if (radio == null || radio < 0)
+		return "";
+	return sep + radio;
+}
+
+function reset_config(phy, radio) {
+	let name = phy + phy_suffix(radio, ".");
+	let prev_config = `/var/run/hostapd-${name}.conf`;
+
+	global.ubus.call('hostapd', 'config_set', { phy, radio, config: '', prev_config });
+	global.ubus.call('wpa_supplicant', 'config_set', { phy, radio, config: []});
+
+	name = phy + phy_suffix(radio, ":");
+	system(`ucode /usr/share/hostap/wdev.uc ${name} set_config '{}'`);
+}
+
+function phy_filename(phy, name) {
+	return `/sys/class/ieee80211/${phy}/${name}`;
+}
+
+function phy_file(phy, name) {
+	return fs.readfile(phy_filename(phy, name));
+}
+
+function phy_index(phy) {
+	return +phy_file(phy, "index");
+}
+
+function phy_path_match(phy, path) {
+	let phy_path = fs.realpath(phy_filename(phy, "device"));
+	return substr(phy_path, -length(path)) == path;
+}
+
+function find_phy_by_path(phys, path) {
+	if (!path)
+		return null;
+
+	path = split(path, "+");
+	phys = filter(phys, (phy) => phy_path_match(phy, path[0]));
+	phys = sort(phys, (a, b) => phy_index(a) - phy_index(b));
+
+	return phys[+path[1]];
+}
+
+function find_phy_by_macaddr(phys, macaddr) {
+	macaddr = lc(macaddr);
+	return filter(phys, (phy) => phy_file(phy, "macaddr") == macaddr)[0];
+}
+
+function find_phy_by_name(phys, name) {
+	return index(phys, name) < 0 ? null : name;
+}
+
+function find_phy(config) {
+	let phys = fs.lsdir("/sys/class/ieee80211");
+
+	return find_phy_by_path(phys, config.path) ??
+	       find_phy_by_macaddr(phys, config.macaddr) ??
+	       find_phy_by_name(phys, config.phy);
+}
+
+function get_channel_frequency(band, channel) {
+	if (channel < 1)
+		return null;
+
+	switch (band) {
+	case '2g':
+		if (channel == 14)
+			return 2484;
+		return 2407 + channel * 5;
+	case '5g':
+		if (channel >= 182 && channel <= 196)
+			return 4000 + channel * 5;
+		return 5000 + channel * 5;
+	case '6g':
+		if (channel == 2)
+			return 5935;
+		return 5950 + channel * 5;
+	case '60g':
+		return 56160 + channel * 2160;
+	}
+}
+
+function setup_phy(phy, config, data) {
+	if (config.channel == "auto")
+		config.channel = 0;
+	config.channel = +config.channel;
+	config.frequency = get_channel_frequency(config.band, config.channel);
+
+	if (config.country) {
+		log(`Setting country code to ${config.country}`);
+		system(`iw reg set ${config.country}`);
+	}
+
+	set_default(config, 'rxantenna', 0xffffffff);
+	set_default(config, 'txantenna', 0xffffffff);
+
+	if (config.txantenna == 'all')
+		config.txantenna = 0xffffffff;
+	if (config.rxantenna == 'all')
+		config.rxantenna = 0xffffffff;
+
+	if (config.txantenna != data?.txantenna || config.rxantenna != data?.rxantenna)
+		reset_config(phy, config.radio);
+
+	netifd.set_data({
+		phy,
+		radio: config.radio,
+		txantenna: config.txantenna,
+		rxantenna: config.rxantenna
+	});
+
+	if (config.txpower)
+		config.txpower = 'fixed ' + config.txpower + '00';
+	else
+		config.txpower = 'auto';
+
+	log(`Configuring '${phy}' txantenna: ${config.txantenna}, rxantenna: ${config.rxantenna} distance: ${config.distance}`);
+	system(`iw phy ${phy} set antenna ${config.txantenna} ${config.rxantenna}`);
+	system(`iw phy ${phy} set distance ${config.distance}`);
+
+	if (config.frag)
+		system(`iw phy ${phy} set frag ${frag}`);
+	if (config.rts)
+		system(`iw phy ${phy} set rts ${rts}`);
+}
+
+function iw_htmode(config) {
+	let suffix = substr(config.htmode, 3);
+	if (suffix == "40+" || suffix == "40-")
+		return "HT" + suffix;
+
+	switch (config.htmode ?? "NONE") {
+	case "HT20":
+	case "VHT20":
+	case "HE20":
+	case "EHT20":
+		return "HT20";
+	case "VHT80":
+	case "HE80":
+	case "EHT80":
+	case "HE160":
+	case "EHT160":
+	case "EHT320":
+		return "80MHZ";
+	case "NONE":
+	case "NOHT":
+		return "NOHT";
+	}
+
+	if (substr(config.htmode, 2) == "40") {
+		switch (config.band) {
+		case "2g":
+			if (+config.channel < 7)
+				return "HT40+";
+			else
+				return "HT40-";
+		default:
+			return ((+config.channel / 4) % 2) ? "HT40+" : "HT40-";
+		}
+	}
+
+	return null;
+}
+
+function config_add(config, name, val) {
+	if (val != null)
+		config[name] = val;
+}
+
+function config_add_mesh_params(config, data) {
+	for (let param in mesh_param_list)
+		config_add(config, param, data[param]);
+}
+
+function setup() {
+	let data = json(ARGV[3]);
+
+	data.phy = find_phy(data.config);
+	if (!data.phy) {
+		log('Bug: PHY is undefined for device');
+		netifd.set_retry(false);
+		return 1;
+	}
+	data.phy_suffix = phy_suffix(data.config.radio, ":");
+	data.vif_phy_suffix = phy_suffix(data.config.radio, ".");
+	let active_ifnames = [];
+
+	log('Starting');
+
+	validate('device', data.config);
+	setup_phy(data.phy, data.config, data.data);
+
+	let supplicant_mesh;
+	let has_ap = false;
+	let idx = {};
+	let supplicant_data = [];
+	let wdev_data = {};
+
+	for (let k, v in data.interfaces) {
+		let mode = v.config.mode;
+		idx[mode] ??= 0;
+		let mode_idx = idx[mode]++;
+
+		if (!v.config.ifname) 
+			v.config.ifname = data.phy + data.vif_phy_suffix + "-" + mode + mode_idx;
+		push(active_ifnames, v.config.ifname);
+
+		if (v.config.encryption == 'owe' && v.config.owe_transition) {
+			mode_idx = idx[mode]++;
+			v.config.owe_transition_ifname = data.phy + data.vif_phy_suffix + "-" + mode + mode_idx;
+			push(active_ifnames, v.config.ifname);
+		}
+
+		switch (mode) {
+		case 'ap':
+			has_ap = true;
+			// fallthrough
+		case 'sta':
+		case 'adhoc':
+		case 'mesh':
+			if (mode != "ap")
+				data.config.noscan = true;
+			validate('iface', v.config);
+			iface.prepare(v.config, data.phy + data.phy_suffix, data.config.num_global_macaddr);
+			netifd.set_vif(k, v.config.ifname);
+			break;
+		}
+
+		switch (mode) {
+		case 'adhoc':
+			if (config.frequency && !v.config.wpa)
+				break;
+			// fallthrough
+		case 'mesh':
+			supplicant_mesh ??= !system("wpa_supplicant -vmesh");
+			if (mode == "mesh" && !supplicant_mesh)
+				break;
+			// fallthrough
+		case 'sta':
+			let config = supplicant.generate(supplicant_data, data, v);
+			if (mode == "mesh")
+				config_add_mesh_params(config, v.config);
+			continue;
+		case 'monitor':
+			break;
+		default:
+			continue;
+		}
+
+		// fallback to wdev setup
+		let config = {
+			mode,
+			ssid: v.config.ssid,
+		};
+
+		if (!v.config.default_macaddr)
+			config.macaddr = v.config.macaddr;
+
+		config_add(config, "htmode", wdev_htmode(data.config));
+		if (mode != "monitor") {
+			config_add(config, "basic-rates", supplicant.ratelist(data.config.basic_rate));
+			config_add(config, "mcast-rate", supplicant.ratestr(v.config.mcast_rate));
+			config_add(config, "beacon-interval", data.config.beacon_int);
+			if (mode == "mesh") {
+				config_add(config, "ssid", v.config.mesh_id);
+				config_add_mesh_params(config, v.config);
+			}
+		}
+
+		wdev_data[v.config.ifname] = config;
+	}
+
+	if (length(supplicant_data) > 0)
+		supplicant.setup(supplicant_data, data);
+
+	if (has_ap)
+		hostapd.setup(data);
+
+	system(`ucode /usr/share/hostap/wdev.uc ${data.phy}${data.phy_suffix} set_config '${printf("%J", wdev_data)}' ${join(' ', active_ifnames)}`);
+
+	if (length(supplicant_data) > 0)
+		supplicant.start(data);
+
+	netifd.set_up();
+
+	return 0
+}
+
+function teardown() {
+	let data = json(ARGV[3]);
+
+	if (!data.data?.phy) {
+		log('Bug: PHY is undefined for device');
+		return 1;
+	}
+
+	log(`Tearing down ${data.data.phy}`);
+
+	reset_config(data.data.phy, data.data.radio);
+
+	return 0;
+}
+
+let ret = 1;
+
+switch(ARGV[1]) {
+case 'dump':
+	ret = dump_options();
+	break;
+
+case 'setup':
+	ret = setup();
+	break;
+
+case 'teardown':
+	ret = teardown();
+	break;
+}
+
+exit(ret);

Quarx2k/OpenWRT-BE7000

commit 928327d136491c46f37b88274847ea12fa0f82e8

wifi-scripts: ucode: fix EAP certificate constraint handling
The supplicant config generator emitted the altsubject_match,
domain_match and domain_suffix_match server certificate constraints
through the plain (unquoted) variable list. As these are UCI arrays,
they were rendered space-separated and without quotes, e.g.

  altsubject_match=DNS:a.example.com DNS:b.example.com

wpa_supplicant parses an unquoted string value as a hex blob, so such a
line fails to parse and the constraint is dropped. wpa_supplicant
expects a single quoted, semicolon-separated string:

  altsubject_match="DNS:a.example.com;DNS:b.example.com"

Join these lists with semicolons and emit them as quoted strings.

The inner-tunnel (phase 2) constraints subject_match2, altsubject_match2,
domain_match2 and domain_suffix_match2 were not written to the config at
all; emit them as well. Add the matching inner EAP-TLS options ca_cert2,
client_cert2, private_key2 and private_key2_passwd to the schema so they
validate cleanly.

Fixes: 98cd55f66663 ("wifi-scripts: add ucode based scripts")
Assisted-by: Claude:claude-opus-4-8
Link: https://github.com/openwrt/openwrt/pull/24088
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/network/config/wifi-scripts/files-ucode/usr/share/ucode/wifi/supplicant.uc
@@ -211,9 +211,24 @@ function setup_sta(data, config) {
 
 	config.mcast_rate = ratestr(config.mcast_rate);
 
+	/*
+	 * Certificate constraint lists are semicolon-separated strings in the
+	 * wpa_supplicant config, while UCI stores them as arrays. Join them here
+	 * so they are emitted as a single quoted value below.
+	 */
+	for (let key in [ 'altsubject_match', 'altsubject_match2',
+			  'domain_match', 'domain_match2',
+			  'domain_suffix_match', 'domain_suffix_match2' ])
+		if (type(config[key]) == 'array')
+			config[key] = length(config[key]) ? join(';', config[key]) : null;
+
 	network_append_string_vars(config, [ 'ssid',
 		'identity', 'anonymous_identity', 'password',
-		'ca_cert', 'ca_cert2', 'client_cert', 'client_cert2', 'subject_match',
+		'ca_cert', 'ca_cert2', 'client_cert', 'client_cert2',
+		'subject_match', 'subject_match2',
+		'altsubject_match', 'altsubject_match2',
+		'domain_match', 'domain_match2',
+		'domain_suffix_match', 'domain_suffix_match2',
 		'private_key', 'private_key_passwd', 'private_key2', 'private_key2_passwd',
 		 ]);
 	network_append_vars(config, [
@@ -222,7 +237,6 @@ function setup_sta(data, config) {
 		'proto', 'mesh_fwding', 'mesh_rssi_threshold', 'frequency', 'fixed_freq',
 		'disable_ht', 'disable_ht40', 'disable_vht', 'vht', 'max_oper_chwidth',
 		'ht40', 'beacon_int', 'ieee80211w', 'rates', 'mesh_basic_rates', 'mcast_rate',
-		'altsubject_match', 'domain_match', 'domain_suffix_match',
 		'bssid_blacklist', 'bssid_whitelist', 'erp', 'eap', 'phase2',
 		'dpp_connector', 'dpp_csign', 'dpp_netaccesskey',
 	]);